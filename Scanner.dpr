465a05120fd2d20414f10afd4b58c7afc18802e2
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2021-05-22 13:03:38 -0300

    Add Dark Theme
    Add Dark Theme
    and TTask with Thread

--- Scanner.dpr
+++ Scanner.dpr
@@ -2,14 +2,18 @@ program Scanner;
 
 uses
   Vcl.Forms,
-  Uscanner in 'Uscanner.pas' {frmScanner};
- // Uthread in 'Uthread.pas';
+  Uscanner in 'Uscanner.pas' {frmScanner},
+  Vcl.Themes,
+  Vcl.Styles;
+
+// Uthread in 'Uthread.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10 Dark');
   Application.CreateForm(TfrmScanner, frmScanner);
   Application.Run;
 end.
--- Uscanner.pas
+++ Uscanner.pas
@@ -6,7 +6,7 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, IdBaseComponent, IdComponent,
   IdTCPConnection, IdTCPClient, Vcl.WinXCtrls, Vcl.StdCtrls, Vcl.ExtCtrls,
-  Vcl.Samples.Spin, IdAntiFreezeBase, IdAntiFreeze,Winsock;
+  Vcl.Samples.Spin, IdAntiFreezeBase, IdAntiFreeze,Winsock, System.Threading;
 
 type
   TfrmScanner = class(TForm)
@@ -15,19 +15,18 @@ type
     seFim: TSpinEdit;
     lblIp: TLabel;
     mmoResultado: TMemo;
-    shp1: TShape;
     lbl1: TLabel;
     lbl2: TLabel;
-    btnIniciar: TButton;
-    lblResultado: TLabel;
+    btnStart: TButton;
     actvtyndctr1: TActivityIndicator;
     lbl3: TLabel;
     idntfrz1: TIdAntiFreeze;
-    btnParar: TButton;
-    procedure btnIniciarClick(Sender: TObject);
-    procedure btnPararClick(Sender: TObject);
+    btnStop: TButton;
+    procedure btnStartClick(Sender: TObject);
+    procedure btnStopClick(Sender: TObject);
   private
     { Private declarations }
+    var tasks : array of TTask;
     procedure IniciarVarredura(const fPortInicio, fPortFim : Integer);
     function PortTCP_IsOpen(dwPort : Word; ipAddressStr: AnsiString) : boolean;
   public
@@ -43,21 +42,26 @@ implementation
 
 {$R *.dfm}
 
-procedure TfrmScanner.btnPararClick(Sender: TObject);
+procedure TfrmScanner.btnStopClick(Sender: TObject);
 begin
   Stop := 1;
-  btnParar.Enabled := False;
+  btnStop.Enabled := False;
 end;
 
-procedure TfrmScanner.btnIniciarClick(Sender: TObject);
+procedure TfrmScanner.btnStartClick(Sender: TObject);
+var
+  aTask: ITask;
 begin
-  try
-    btnParar.Enabled := True;
-    actvtyndctr1.Animate := True;
-    IniciarVarredura(seInicio.Value, seFim.value);
-  finally
-    actvtyndctr1.Animate := False;
-  end;
+  aTask:= TTask.Create(
+  procedure
+  begin
+      TThread.Synchronize(TThread.CurrentThread,
+       procedure
+       begin
+           IniciarVarredura(seInicio.Value, seFim.value);
+       end);
+   end);
+  aTask.Start;
 end;
 
 procedure TfrmScanner.IniciarVarredura(const fPortInicio, fPortFim: Integer);
@@ -65,70 +69,70 @@ var
   iport : Integer;
    ip :  AnsiString;
 begin
-  Stop:= 0 ;
-  iport:= fPortInicio;
-  ip := AnsiString(edtIp.Text);
+  btnStart.Enabled := True;
+  actvtyndctr1.Animate := True;
+  try
+      Stop:= 0 ;
+      iport:= fPortInicio;
+      ip := AnsiString(edtIp.Text);
 
-  mmoResultado.lines.Clear;
-  mmoResultado.lines.Add('Iniciando Varredura de portas, '+
-                         'disparada para o host: ' + edtIp.text);
-  mmoResultado.lines.add(#13);
-
-  while iport <= fPortFim do
-  Begin
-    Application.ProcessMessages;
-
-    if Stop = 1 then
-    begin
-      mmoResultado.lines.Add('Verificação cancelada pelo usuário!');
-      break;
-    end;
-
-    mmoResultado.lines.Add('Verificando status da porta: [ '+ IntToStr(iport)+' ] '+
-                           'para o host: ' + edtIp.text);
-
-    Application.ProcessMessages;
-
-    Try
-      if PortTCP_IsOpen(iport, PAnsiChar(ip)) then
-        mmoResultado.lines.add('* OPEN * Porta [ '+ inttostr(iport)+ ' ] está Aberta.')
-      else
-        mmoResultado.lines.add('A Porta [ '+ inttostr(iport)+ ' ] está Fechada.');
+      mmoResultado.lines.Clear;
+      mmoResultado.lines.Add('Starting verificartion of port, '+
+                             'for host: ' + edtIp.text);
       mmoResultado.lines.add(#13);
-    Finally
-      Inc(iport);
-      seInicio.Value := iport;
-      Application.ProcessMessages;
-    end;
-  end;
 
-  mmoResultado.lines.add(#13);
-  seInicio.Value := 1;
-  mmoResultado.lines.add('Verificação Terminada.');
+      while iport <= fPortFim do
+      Begin
+        Application.ProcessMessages;
+
+        if Stop = 1 then
+        begin
+          mmoResultado.lines.Add('Verification terminated by user');
+          break;
+        end;
+
+        Application.ProcessMessages;
+
+        Try
+          if PortTCP_IsOpen(iport, PAnsiChar(ip)) then
+            mmoResultado.lines.add('* OPEN * The port '+ inttostr(iport)+ ' are Open .')
+          else
+            mmoResultado.lines.add('The port '+ inttostr(iport)+ ' are Closed.');
+        Finally
+          Inc(iport);
+          seInicio.Value := iport;
+          Application.ProcessMessages;
+        end;
+      End;
+
+  finally
+    mmoResultado.lines.add(#13);
+    seInicio.Value := 1;
+    mmoResultado.lines.add('Verification completed.');
+    actvtyndctr1.Animate := False;
+    btnStop.Enabled := False;
+  end;
 end;
 
-function TfrmScanner.PortTCP_IsOpen(dwPort: Word;
-  ipAddressStr: AnsiString): boolean;
+function TfrmScanner.PortTCP_IsOpen(dwPort: Word;ipAddressStr: AnsiString): boolean;
 var
   client : sockaddr_in;
   sock   : Integer;
-
   ret    : Integer;
   wsdata : WSAData;
 begin
- Result:=False;
- ret := WSAStartup($0002, wsdata); //initiates use of the Winsock DLL
+  Result:=False;
+  ret := WSAStartup($0002, wsdata); //initiates use of the Winsock DLL
   if ret<>0 then exit;
   try
     client.sin_family      := AF_INET;  //Set the protocol to use , in this case (IPv4)
     client.sin_port        := htons(dwPort); //convert to TCP/IP network byte order (big-endian)
     client.sin_addr.s_addr := inet_addr(PAnsiChar(ipAddressStr));  //convert to IN_ADDR  structure
-    sock  :=socket(AF_INET, SOCK_STREAM, 0);    //creates a socket
-    Result:=connect(sock,client,SizeOf(client))=0;  //establishes a connection to a specified socket
+    sock                   := socket(AF_INET, SOCK_STREAM, 0);    //creates a socket
+    Result                 := connect(sock,client,SizeOf(client))=0;  //establishes a connection to a specified socket
   finally
-  WSACleanup;
+    WSACleanup;
   end;
-
 end;
 
 end.
